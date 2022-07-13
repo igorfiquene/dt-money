9d7b4570b8a2e03832bb4abbd366ca9ebfac9106
igorfiquene <igorfpessoa@gmail.com>
2022-07-12 23:50:09 -0300

    component: dashboard and summary created

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import { Container } from "./styles";
+import incomeImg from '../../assets/images/income.svg'
+import outcomeImg from '../../assets/images/outcome.svg'
+import total from '../../assets/images/total.svg'
+
+export function Summary() {
+	return (
+		<Container>
+			<div>
+				<header>
+					<p>Entradas</p>
+					<img src={incomeImg} alt="Entradas" />
+				</header>
+
+				<strong>R$1000,00</strong>
+			</div>
+
+			<div>
+				<header>
+					<p>Saídas</p>
+					<img src={outcomeImg} alt="Saídas" />
+				</header>
+
+				<strong> - R$500,00</strong>
+			</div>
+
+			<div>
+				<header>
+					<p>Total</p>
+					<img src={total} alt="Total" />
+				</header>
+
+				<strong> R$500,00</strong>
+			</div>
+		</Container>
+	)
+}
